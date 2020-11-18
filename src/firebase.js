dcfa62086d978780482913a5bbb04b8d3e903712
Rathorepradhumna <42332858+Rathorepradhumna@users.noreply.github.com>
2020-11-18 16:34:21 +0530

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAJpOSTt7QbqU1A8UAX6oVuDgKkf4-EojY",
-    authDomain: "facebook-clone-e9849.firebaseapp.com",
-    databaseURL: "https://facebook-clone-e9849.firebaseio.com",
-    projectId: "facebook-clone-e9849",
-    storageBucket: "facebook-clone-e9849.appspot.com",
-    messagingSenderId: "220583767246",
-    appId: "1:220583767246:web:d67aeb4d01e9475275749f"
-  };
